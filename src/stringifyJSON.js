68f691d8de3b6aee8d832a314aa85b18cc8208fc
Kim Ha <kimberlygha@gmail.com>
2015-09-13 12:34:59 -0700

    Created conditions for arrays, objects, and primitives

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,26 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if(typeof obj !== "object"){
+    if(typeof obj == "number"){
+      return obj.toString();
+    }
+    else{
+      return "\""+obj+"\"";
+    }
+  }
+  else if(Array.isArray(obj)){
+    var arr = "[";
+    for(var i=0; i<obj.length;i++){
+      arr = arr+ (stringifyJSON(obj[i]))+",";
+    }
+    return arr +"]"; 
+  }
+  else{
+    var curly = "{";
+    for(var key in obj){
+      curly= curly+"\""+key+"\":"+ stringifyJSON(obj[key])+",";
+    }
+    return curly+"}";
+  }
 };
