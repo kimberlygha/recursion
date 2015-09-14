3ba02cc779469f251eb509effe8b2fa387453e32
Kim Ha <kimberlygha@gmail.com>
2015-09-14 14:37:05 -0700

    fixed loop - completed classname function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -8,18 +8,16 @@ var getElementsByClassName = function(className){
   var classElements = [];
   function recurse(element){
     var children = element.childNodes;
+    console.log(element);
     if(element.classList.contains(className)){
-      console.log(element);
       classElements.push(element);
     }
-    for(var i=0; i<children.length-1;i++){
-      if(children[i].nodeType != 3 && children[i].nodeTYpe != 8){
+    for(var i=0; i<children.length;i++){
+      if(children[i].nodeType != 3 && children[i].nodeType != 8){
         recurse(children[i]);
       }
     }
   }
   recurse(document.body);
-  console.log(classElements)
-  console.log(document.getElementsByClassName(className));
   return classElements;
 };
