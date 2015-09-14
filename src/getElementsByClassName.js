a4b6b03b27d51775e5fd5199f304491961223f52
Kim Ha <kimberlygha@gmail.com>
2015-09-14 09:01:12 -0700

    Created skeleton - defined recursive function inside, added base condition and recursive condition

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,19 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ){
-  // your code here
+  var classElements = [];
+  function recurse(element){
+    if(element.childNodes.length == 0){
+      for(var i=0; i<element.classList.length-1; i++){
+        if(element.classList[i] == className){
+          classElements.push(element); 
+        }
+      }
+    }
+    for(var x=0; x<element.childNodes.length-1; x++){
+      recurse(element.childNodes[x]);
+    }
+  }
+  recurse(document.body);
+  return classElements
 };
