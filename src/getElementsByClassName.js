9d51a0d181405af1c2a2602333c181d59f2a6c98
Kim Ha <kimberlygha@gmail.com>
2015-09-14 12:34:25 -0700

    Filter text and comment nodes

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,21 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   var classElements = [];
   function recurse(element){
-    if(element.childNodes.length == 0){
-      for(var i=0; i<element.classList.length-1; i++){
-        if(element.classList[i] == className){
-          classElements.push(element); 
-        }
-      }
+    var children = element.childNodes;
+    if(element.classList.contains(className)){
+      console.log(element);
+      classElements.push(element);
     }
-    for(var x=0; x<element.childNodes.length-1; x++){
-      recurse(element.childNodes[x]);
+    for(var i=0; i<children.length-1;i++){
+      if(children[i].nodeType != 3 && children[i].nodeTYpe != 8){
+        recurse(children[i]);
+      }
     }
   }
   recurse(document.body);
-  return classElements
+  console.log(classElements)
+  console.log(document.getElementsByClassName(className));
+  return classElements;
 };
